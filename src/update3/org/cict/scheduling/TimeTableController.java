9ffad1a300d36ed9131bb635ff253c7d3a8b599c
jhmvin <jhmvinperello@gmail.com>
2017-09-28 10:50:18 +0800

    Schedule Table

--- src/update3/org/cict/scheduling/TimeTableController.java
+++ src/update3/org/cict/scheduling/TimeTableController.java
@@ -23,6 +23,7 @@
  */
 package update3.org.cict.scheduling;
 
+import com.jhmvin.Mono;
 import com.jhmvin.flow.MonoLoop;
 import com.jhmvin.fx.display.ControllerFX;
 import com.jhmvin.fx.display.SceneFX;
@@ -42,28 +43,31 @@ import update3.org.cict.ScheduleConstants;
  * @author Jhon Melvin
  */
 public class TimeTableController extends SceneFX implements ControllerFX {
-    
+
     @FXML
     private StackPane application_root;
-    
+
     @FXML
     private HBox hbox_table;
-    
+
     public TimeTableController() {
     }
-    
+
     private final Double LINE_DISTANCE = 14.00;
     private final Double LINE_INTERVAL = 04.00;
     private Double UNIT;
-    
+
     @Override
     public void onInitialization() {
         this.UNIT = LINE_DISTANCE * LINE_INTERVAL;
-        
+
+        /**
+         * Add listener for vertical resizing.
+         */
         hbox_table.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
             Double height = (Double) newValue;
             Double single_unit = height / this.UNIT;
-            
+
             hbox_table.getChildren().forEach(column -> {
                 if (column instanceof VBox) {
                     VBox col = (VBox) column;
@@ -75,56 +79,80 @@ public class TimeTableController extends SceneFX implements ControllerFX {
                 }
             });
         });
-        
+
+        /**
+         * Create columns from time to Sunday.
+         */
         MonoLoop.range(1, 8, 1, loop -> {
+            // column
             VBox column = new VBox();
             HBox.setHgrow(column, Priority.ALWAYS);
-            column.getStyleClass().add("column-day");
+            if (loop.getValue().equals(1)) {
+                column.getStyleClass().add("column-template-lefter");
+            } else {
+                column.getStyleClass().add("column-template");
+            }
+
             hbox_table.getChildren().add(column);
-            
-            HBox header = new HBox();
-            header.getStyleClass().add("row");
-            header.setMaxWidth(Double.MAX_VALUE);
-            Integer s = 4;
-            header.setUserData(s);
-            Label head = new Label(getTableHeader().get(loop.getValue() - 1));
-            header.getChildren().add(head);
-            
-            column.getChildren().add(header);
-            
-            AtomicInteger counter = new AtomicInteger(3);
-            ScheduleConstants.getTimeLapsePretty().forEach(action -> {
-                if (counter.get() != 3) {
-                    counter.incrementAndGet();
-                    return;
-                }
-                HBox row = new HBox();
-                row.getStyleClass().add("row");
-                row.setMaxWidth(Double.MAX_VALUE);
+
+            // column header
+            TimeTableCell cellHeader = new TimeTableCell();
+            cellHeader.pane.getStyleClass().add("row-template-header");
+            cellHeader.pane.setMaxWidth(Double.MAX_VALUE);
+            cellHeader.pane.setUserData(Integer.valueOf("4"));
+            cellHeader.content.setText(getTableHeader().get(loop.getValue() - 1));
+
+            // add header to column
+            column.getChildren().add(cellHeader.pane);
+
+            ArrayList<String> timeLapse = ScheduleConstants.getTimeLapsePretty();
+            for (Integer start = 0; start < timeLapse.size(); start += 4) {
+                TimeTableCell cell = new TimeTableCell();
+                cell.pane.getStyleClass().add("row-template");
                 Integer multiplier = 4;
-                row.setUserData(multiplier);
-                Label time = new Label(action);
-                row.getChildren().add(time);
-                column.getChildren().add(row);
-                
-                counter.set(0);
-                
-            });
-            
+                cell.pane.setUserData(multiplier);
+                try {
+                    cell.content.setText(timeLapse.get(start) + " - " + timeLapse.get(start + 4));
+                    if (!loop.getValue().equals(1)) {
+                        cell.content.getStyleClass().add("row-template-text");
+                    }
+                    column.getChildren().add(cell.pane);
+                } catch (Exception e) {
+                }
+
+            }
+
         });
-        
+
     }
-    
+
     private ArrayList<String> getTableHeader() {
         ArrayList<String> headerText = new ArrayList<>();
         headerText.add("Time");
         headerText.addAll(ScheduleConstants.getDayList());
         return headerText;
     }
-    
+
     @Override
     public void onEventHandling() {
-        
+
     }
-    
+
+    private class TimeTableCell extends SceneFX {
+
+        private HBox pane;
+        private Label content;
+
+        public TimeTableCell() {
+            pane = Mono.fx().create()
+                    .setPackageName("update3.org.cict.scheduling")
+                    .setFxmlDocument("TableCell")
+                    .makeFX()
+                    .pullOutLayout();
+
+            content = super.searchAccessibilityText(pane, "lbl_content");
+        }
+
+    }
+
 }
