ae3dd6a57f5de6c52cc1c6fc560eaa525028b70b
joemardc98 <dlcrzjoemz12@gmail.com>
2018-01-05 13:24:36 +0800

    added saving of info in retention

--- src/org/cict/evaluation/FailedGradeChecker.java
+++ src/org/cict/evaluation/FailedGradeChecker.java
@@ -30,6 +30,8 @@ import app.lazy.models.CurriculumSubjectMapping;
 import app.lazy.models.DB;
 import app.lazy.models.Database;
 import app.lazy.models.GradeMapping;
+import app.lazy.models.RetentionPolicyMapping;
+import app.lazy.models.RetentionSubjectMapping;
 import app.lazy.models.StudentMapping;
 import app.lazy.models.SubjectMapping;
 import com.itextpdf.text.Document;
@@ -38,6 +40,7 @@ import com.jhmvin.fx.async.Transaction;
 import java.util.ArrayList;
 import org.apache.commons.lang3.text.WordUtils;
 import org.cict.PublicConstants;
+import org.cict.authentication.authenticator.CollegeFaculty;
 import org.cict.authentication.authenticator.SystemProperties;
 import org.cict.reports.ReportsDirectory;
 import org.cict.reports.retention.RetentionLetter;
@@ -51,7 +54,7 @@ public class FailedGradeChecker extends Transaction{
 
     private StudentMapping student;
     private AcademicTermMapping currentAcadTerm;
-    private AcademicTermMapping prevAcadTerm;
+    private AcademicTermMapping prevAcadTerm = new AcademicTermMapping();
     private CurriculumMapping curriculum;
     private AcademicProgramMapping acadProgram;
 
@@ -66,6 +69,8 @@ public class FailedGradeChecker extends Transaction{
     public String getLog() {
         return log;
     }
+    
+    private Integer year = 1, semester = 1;
     @Override
     protected boolean transaction() {
         if(student == null) {
@@ -91,7 +96,7 @@ public class FailedGradeChecker extends Transaction{
             return false;
         }
         
-        int year = 1, semester = 1;
+        
         if(currentAcadTerm.getSemester_regular().equals(1)) {
             year = (student.getYear_level()-1);
             semester = 2;
@@ -110,7 +115,7 @@ public class FailedGradeChecker extends Transaction{
                 }
                 prep_curriculum = Database.connect().curriculum().getPrimary(student.getPREP_id());
             }
-            if(year == 3) {
+            if(year.equals(3)) {
                 curriculum = prep_curriculum;
             }
         }
@@ -142,6 +147,7 @@ public class FailedGradeChecker extends Transaction{
                 prevAcadTermID = grade.getACADTERM_id();
                 System.out.println("IT'S FAILED " + subject.getCode());
                 failedSubject++;
+                failedSubject_lst.add(subject);
             }
         }
         if(currentAcadTerm.getSemester_regular().equals(2)) {
@@ -153,6 +159,12 @@ public class FailedGradeChecker extends Transaction{
             if(currentAcadTerm.getSemester_regular().equals(2)) {
                prevAcadTerm.setSemester_regular(1);
                prevAcadTerm.setSemester("FIRST SEMESTER");
+            }
+        } else {
+            if(currentAcadTerm.getSemester_regular().equals(1)) {
+               prevAcadTerm.setSemester_regular(2);
+               prevAcadTerm.setSemester("SECOND SEMESTER");
+               prevAcadTerm.setSchool_year("");
             }
         }
         if(failedSubject>=2) {
@@ -178,6 +190,8 @@ public class FailedGradeChecker extends Transaction{
         " Thirteen",
         " Fourteen",
         " Fifteen"};
+    
+    private ArrayList<SubjectMapping> failedSubject_lst = new ArrayList<>();
     @Override
     protected void after() {
         // print if qualified for retention letter
@@ -192,6 +206,45 @@ public class FailedGradeChecker extends Transaction{
             if(prevAcadTerm != null) {
                 System.out.println(prevAcadTerm.getSchool_year() + " Semester " + (prevAcadTerm.getSemester_regular()==1? "1st" : (prevAcadTerm.getSemester_regular()==2? "2nd" : "Midyear")));
             }
+            
+            RetentionPolicyMapping existing = Mono.orm().newSearch(Database.connect().retention_policy())
+                    .eq(DB.retention_policy().CURRICULUM_id, curriculum.getId())
+                    .eq(DB.retention_policy().STUDENT_id, this.student.getCict_id())
+                    .eq(DB.retention_policy().academic_semester, currentAcadTerm.getSemester())
+                    .eq(DB.retention_policy().academic_year, currentAcadTerm.getSchool_year())
+                    .eq(DB.retention_policy().section, section)
+                    .eq(DB.retention_policy().semester, prevAcadTerm.getSemester())
+                    .eq(DB.retention_policy().year_level, this.getYearLevel(year))
+                    .active(Order.desc(DB.retention_policy().id)).first();
+            if(existing == null) {
+                // create a retentionPolicyMap for the record of the student
+                RetentionPolicyMapping retentionPolicy = new RetentionPolicyMapping();
+                retentionPolicy.setActive(1);
+                retentionPolicy.setAcademic_year(currentAcadTerm.getSchool_year());
+                retentionPolicy.setAcademic_semester(currentAcadTerm.getSemester());
+                retentionPolicy.setCURRICULUM_id(curriculum.getId());
+                retentionPolicy.setSTUDENT_id(this.student.getCict_id());
+                retentionPolicy.setSection(section);
+                retentionPolicy.setSemester(prevAcadTerm.getSemester());
+                retentionPolicy.setVerified_by(CollegeFaculty.instance().getFACULTY_ID());
+                retentionPolicy.setYear_level(this.getYearLevel(year));
+                int retentionPolicy_id = Database.connect().retention_policy().insert(retentionPolicy);
+                if(retentionPolicy_id != -1) {
+                    for(SubjectMapping each : failedSubject_lst) {
+                        RetentionSubjectMapping retentionSubject = new RetentionSubjectMapping();
+                        retentionSubject.setActive(1);
+                        retentionSubject.setRetention_id(retentionPolicy_id);
+                        retentionSubject.setSubject_code(each.getCode());
+                        retentionSubject.setSubject_title(each.getDescriptive_title());
+                        retentionSubject.setUnits((each.getLab_units() + each.getLec_units()) + "");
+                        int res = Database.connect().retention_subject().insert(retentionSubject);
+                    }
+                } else {
+                    System.out.println("RETENTION POLICY & SUBJECT NOT SAVED");
+                }
+            }
+            
+            //print letter
             this.printeLetter(name, section, failed);
         }
         super.after(); //To change body of generated methods, choose Tools | Templates.
@@ -250,4 +303,17 @@ public class FailedGradeChecker extends Transaction{
         this.document = parentStage;
     }
     
+    private String getYearLevel(Integer yrlvl) {
+        switch(yrlvl) {
+            case 1:
+                return "FIRST YEAR";
+            case 2:
+                return "SECOND YEAR";
+            case 3:
+                return "THIRD YEAR";
+            case 4:
+                return "FOURTH YEAR";
+        }
+        return null;
+    }
 }
